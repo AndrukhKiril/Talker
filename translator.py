bc5e3d70dc9661f5e74cba8d7b5d571618d67e01
AndrukhKiril <kirilandrukh@gmail.com>
2024-03-01 22:30:25 +0100

    Added translator

--- translator.py
+++ translator.py
@@ -1,4 +1,5 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+from datasets import load_dataset
 
 model = AutoModelForSeq2SeqLM.from_pretrained("facebook/nllb-200-distilled-600M")
 tokenizer = AutoTokenizer.from_pretrained("facebook/nllb-200-distilled-600M")
@@ -8,15 +9,3 @@ def translate(text, src_lang, tgt_lang):
     print(src_lang, tgt_lang)
     translated_text = translator(text)[0]['translation_text']
     return translated_text
-
-if __name__ == "__main__":
-    translate('''Вічний революціонер –
-                Дух, що тіло рве до бою,
-                Рве за поступ, щастя й волю, –
-                Він живе, він ще не вмер.
-                Ні попівськії тортури,
-                Ні тюремні царські мури,
-                Ані війська муштровані,
-                Ні гармати лаштовані,
-                Ні шпіонське ремесло
-                В гріб його ще не звело''')
